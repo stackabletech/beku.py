43bb39cf8f57d33c617fa7fd6dc36b337be7a8ef
Nick Larsen <nick.larsen@stackable.tech>
2024-05-13 09:59:32 +0200

    fix(nix): remove remaining references to pkg

--- beku.nix
+++ beku.nix
@@ -3,10 +3,10 @@ let
   # I wish there was a better way. If the command fails, it is very hard to see 
   # that the problem is here. Perhaps there is some way to evaluate it in python
   # and reference __version__ directly?
-  version = builtins.readFile (pkgs.runCommand "foo" { src = [ ./src ]; } ''
+  version = builtins.readFile (runCommand "foo" { src = [ ./src ]; } ''
     grep -E '^__version__' $src/beku/version.py | grep -o '".*"' | tr -d \" > $out
   '');
-  manifest = (pkgs.lib.importTOML ./pyproject.toml).project;
+  manifest = (lib.importTOML ./pyproject.toml).project;
 in
 python3.pkgs.buildPythonApplication {
   pname = manifest.name;
